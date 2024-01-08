da179a5b37860ea315ff7995f954978768c194b6
KENC0DE <kenansameseret@gmail.com>
2024-01-09 00:53:51 +0530

    add

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@ The app
 """
 
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -25,6 +25,12 @@ if not porter:
 def teardown(error):
     """Clean"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ Page Not found"""
+    return jsonify({'error': 'Not found'}), 404
 
 
 if __name__ == '__main__':
